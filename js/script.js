606089aa7bd0764406bf7ab6e2b9dd8c966a81e1
Surikat228 <awf2014@yandex.ru>
2021-06-06 13:13:52 +0300

    Адаптирую футер + пытаюсь перенести копирайт

--- js/script.js
+++ js/script.js
@@ -9,4 +9,13 @@ $(function () {
 		$('.bg-prlx__mountains').css('background-position-y', (wScroll * -0.1 + 'px'));
 	}
 
+	///////////////////  Vanilla JS  ////////////////////////
+
+	const copyright = document.querySelector('.footleftbx__copyright');
+	const footerbody = document.querySelector('.footer__body');
+
+	if (window.matchMedia('(max-width: 992px)').matches) {
+		copyright.append(footerbody)
+	};
+
 });
